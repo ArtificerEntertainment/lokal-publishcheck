613d120d30856b64ff6f348de2c8f481249a3f09
Artificer Entertainment <opensource@artificer.ca>
2016-02-24 21:40:24 -0500

    Updated test to v1.1.1.

--- test/test.js
+++ test/test.js
@@ -5,11 +5,11 @@ const lokal = require('lokal');
 
 describe('local dependencies', () => {
   it('dependency0 is loaded properly', () => {
-    assert.equal(lokal.dependency0, 'dependency0 inside lokal-publishcheck');
+    assert.equal(lokal('dependency0'), 'dependency0 inside lokal-publishcheck');
   });
 
   it('dependency1 is loaded properly', () => {
-    assert.equal(lokal.dependency1, 'dependency1 inside lokal-publishcheck');
+    assert.equal(lokal('dependency1'), 'dependency1 inside lokal-publishcheck');
   });
 
 });
